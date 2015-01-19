61f5cb3c10aa094d53b1c3a1d0c4d44903ac458e
Marcin Miszczyk <mpmiszczyk@gmail.com>
2015-01-19 14:43:49 +0100

    Remove dead code.

--- c_src/c_randerl.c
+++ c_src/c_randerl.c
@@ -120,8 +120,6 @@ threads_count() {
   ErlNifSysInfo sys_info;
   enif_system_info(&sys_info, sizeof(sys_info));
   int scheduler_threads  = sys_info.scheduler_threads;
-  int dirty_threads = sys_info.dirty_scheduler_support ? scheduler_threads : 0;
   int driver_threads = sys_info.async_threads;
-  printf("Schedulers from C: %d + %d + %d  \n", scheduler_threads, dirty_threads, driver_threads);
-  return scheduler_threads + dirty_threads + driver_threads;
+  return scheduler_threads + driver_threads;
 }
